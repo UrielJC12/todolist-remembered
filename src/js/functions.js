f175bdda62c25766920ba3e11f4cd41b676a406c
Uriel Javier Carreño <urieljavier236@gmail.com>
2025-12-27 15:21:22 -0600

    v1.03

--- src/js/functions.js
+++ src/js/functions.js
@@ -52,3 +52,46 @@ themeButton.addEventListener('click', () => {
     htmlElement.setAttribute('data-theme', newTheme);
     localStorage.setItem('theme', newTheme);
 });
+
+
+//notebook funtionality
+
+const inutBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask(){
+    if (inutBox.value === ''){
+        alert("You need to add something");
+
+    } 
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = inutBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inutBox.value = ""
+    saveData();
+}
+
+listContainer.addEventListener("click", (e) => {
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    } else if (e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+
+}, false);
+
+const saveData = () => {
+    localStorage.setItem('data', listContainer.innerHTML);
+}
+
+const showTask = () => {
+    listContainer.innerHTML = localStorage.getItem('data');
+}
+showTask();
